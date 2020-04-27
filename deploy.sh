870acd5c0157b7e8a7698d90974c5417c5cd9b3e
Roger Gonzalez <roger@rogs.me>
2020-04-27 10:48:43 -0300

    Changed the deployment process to not render drafts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf ~/code/personal/blog.rogs.me/public/*
-hugo -D
+hugo
 ssh root@cloud.rogs.me "rm -rf /var/www/blog.rogs.me/*"
 scp -r ~/code/personal/blog.rogs.me/public/* root@cloud.rogs.me:/var/www/blog.rogs.me
 ssh root@cloud.rogs.me "sudo service nginx restart"

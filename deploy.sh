135f66bfb806bc43cc347934fe8de9d847cba461
Roger Gonzalez <roger@rogs.me>
2023-01-02 16:44:40 -0300

    Removed old deploy script

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf ~/code/personal/rogs.me/public/*
-hugo
-rsync -vruP ~/code/personal/rogs.me/public/* root@cloud.rogs.me:/var/www/rogs.me
-ssh root@cloud.rogs.me "sudo service nginx restart"

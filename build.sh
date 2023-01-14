1560f13531a8a18a1f0e10f99652d437a6f13129
Roger Gonzalez <roger@rogs.me>
2023-01-14 16:46:44 -0300

    Converting jpgs to webp automatically

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 find ./ -type f -name '*.png' -not -path '*/.git/*' -exec sh -c 'cwebp -lossless $1 -o "${1%.png}.webp"' _ {} \;
+find ./ -type f -name '*.jpg' -not -path '*/.git/*' -exec sh -c 'cwebp -lossless $1 -o "${1%.jpg}.webp"' _ {} \;
 find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.png/\.webp/g' {} \;
+find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.jpg/\.webp/g' {} \;
 rm -rf /var/www/rogs.me/*
 hugo -s . -d /var/www/rogs.me/

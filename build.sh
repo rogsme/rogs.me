21dc07e355f33658404abc0533a8bf93f154272a
Roger Gonzalez <roger@rogs.me>
2023-01-15 15:20:54 -0300

    Added jpeg files to the conversion step

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 find ./ -type f -name '*.png' -not -path '*/.git/*' -exec sh -c 'cwebp -lossless $1 -o "${1%.png}.webp"' _ {} \;
 find ./ -type f -name '*.jpg' -not -path '*/.git/*' -exec sh -c 'cwebp -lossless $1 -o "${1%.jpg}.webp"' _ {} \;
+find ./ -type f -name '*.jpeg' -not -path '*/.git/*' -exec sh -c 'cwebp -lossless $1 -o "${1%.jpeg}.webp"' _ {} \;
 find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.png/\.webp/g' {} \;
 find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.jpg/\.webp/g' {} \;
+find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.jpeg/\.webp/g' {} \;
 rm -rf /var/www/rogs.me/*
 hugo -s . -d /var/www/rogs.me/

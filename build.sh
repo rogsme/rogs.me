8f9e61b2778d68395d471c83d233a0a7bf22847b
Roger Gonzalez <roger@rogs.me>
2023-01-19 22:22:22 -0300

    Changed hugo build command

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ find ./ -type f -name '*.jpeg' -not -path '*/.git/*' -exec sh -c 'cwebp -lossles
 find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.png/\.webp/g' {} \;
 find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.jpg/\.webp/g' {} \;
 find . -type f -not -path '*/.git/*' -exec sed -i -e 's/\.jpeg/\.webp/g' {} \;
-rm -rf /var/www/rogs.me/*
-hugo -s . -d /var/www/rogs.me/
+
+hugo -s . -d /var/www/rogs.me/ --minify --cleanDestinationDir --gc --verbose
